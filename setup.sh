0eb27a93d282a90ff48dbb8cda1502c603bea381
Jacob Hayes <jacob.r.hayes@gmail.com>
2018-01-08 01:35:52 -0600

    Update base setup script

--- setup.sh
+++ setup.sh
@@ -1,35 +1,32 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -o nounset
+set -o pipefail
 
-echo "Installing homebrew"
-brew -h || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! hash brew 2>/dev/null; then
+    echo "Installing homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+xcode-select --install
+echo "Press enter once Xcode Command Line Tools are installed..."
+read -r
 
 echo "Installing brew packages"
 xargs brew install      < ./requirements/brew-requirements.txt
 echo "Installing brew cask apps"
 xargs brew cask install < ./requirements/cask-requirements.txt
-echo "Installing from commands"
-sh ./requirements/manual-requirements.sh
-
-echo "Setting up nvm"
-mkdir -p ~/.nvm # Setup nvm's working directory
-export NVM_DIR=~/.nvm
-set +o errexit # Something in there fails, but is ok.
-source $(brew --prefix nvm)/nvm.sh
-set -o errexit
-
-echo "Installing latest node"
-nvm install node
 
 echo "Updating pip"
-pip install --upgrade pip setuptools
+pip2 install --upgrade pip setuptools
 pip3 install --upgrade pip setuptools
-echo "Installing pip packages"
-pip install -r ./requirements/pip-requirements.txt
+echo "Installing pip3 packages"
+pip3 install -r ./requirements/pip3-requirements.txt
 
-echo "Creating docker machine"
-docker-machine status default || docker-machine create -d virtualbox --virtualbox-memory "4096" --virtualbox-disk-size "102400" default # 4GB RAM, 100GB storage
+echo "Linking dotfiles to ~/"
+# Using the gnu version of cp from brew coreutils b/c it has the -s flag for symbolic links and works with directories
+gcp -srf "${PWD}"/dotfiles/.[^.]* ~/
 
 echo "Installing Vundle"
 test -d ~/.vim/bundle/Vundle.vim  || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -39,17 +36,16 @@ vim +PluginInstall +qall
 echo "Installing TmuxPluginManager"
 test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Start tmux and run 'prefix-I' to install tmux plugins... Press enter when done..."
-read
+read -r
 
-echo "Changing default shell to brew's zsh"
-sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-chsh -s /usr/local/bin/zsh
+echo "Changing default shell to brew's fish"
+sudo sh -c "echo '/usr/local/bin/fish' >> /etc/shells"
+chsh -s /usr/local/bin/fish
 
-echo "Linking dotfiles to ~/"
-# Using the gnu version of cp from brew coreutils b/c it has the -s flag for symbolic links and works with directories
-gcp -srf ${PWD}/dotfiles/.[^.]* ~/
+if hash gcloud 2>/dev/null; then
+    echo 'y' | gcloud components install docker-credential-gcr
+fi
 
-echo "Allowing repeated keypresses"
-defaults write -g ApplePressAndHoldEnabled -bool false
+./macos_settings.sh
 
 echo "Done!"
