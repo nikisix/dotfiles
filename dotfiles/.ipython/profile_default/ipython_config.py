6a464731c5d2e9f4cacd66f947ce775e51f27e9d
Nick Tomasino <nicolai.tesela@gmail.com>
2021-10-25 12:25:19 -0500

    adding ipython config that doesnt quite work yet

--- dotfiles/.ipython/profile_default/ipython_config.py
+++ dotfiles/.ipython/profile_default/ipython_config.py
@@ -1,1 +1,4 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.extra_open_editor_shortcuts=True
+c.InteractiveShellApp.extensions.append('autoreload')
+c.extensions.autoreload = 2
