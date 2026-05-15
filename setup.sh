cb425df2378b912a97d6eeb056f6f50da6a5518e
Nick Tomasino <nick.w.tomasino@gmail.com>
2026-05-15 14:17:35 -0700

    adding and removing installed programs

--- setup.sh
+++ setup.sh
@@ -6,25 +6,21 @@ set -o pipefail
 
 if ! hash brew 2>/dev/null; then
     echo "Installing homebrew"
-    #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$(USERNAME)/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
- 
+
 echo "Installing brew and cask packages"
 brew bundle --file=requirements/Brewfile
 
-# echo "Updating pip"
-# pip3 install --upgrade pip setuptools
 echo "Installing python packages"
-# pip3 install --upgrade -r ./requirements/python-requirements.txt
-# not recommended to global install with pip anymore
-# use pipx instead
 for pkg in $(grep -v '^#' requirements/python-requirements.txt | grep -v '^$'); do
     echo "Installing $pkg...";
     pipx install $pkg;
 done
+pipx ensurepath
+source ~/.zshrc
 
 echo "Linking dotfiles to ~/"
 # Using the gnu version of cp from brew coreutils (in gnubin) b/c it has the -s flag for symbolic links and works with directories
@@ -37,18 +33,15 @@ test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.
 echo "Start tmux and run 'prefix-I' to install tmux plugins... Press enter when done..."
 read -r
 
-echo "Changing default shell to brew's zsh"
-sudo sh -c "echo '/opt/homebrew/bin/zsh' >> /etc/shells"
-chsh -s /opt/homebrew/bin/zsh
 echo "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-if hash gcloud 2>/dev/null; then
-    echo 'y' | gcloud components install docker-credential-gcr
-fi
+# if hash gcloud 2>/dev/null; then
+#     echo 'y' | gcloud components install docker-credential-gcr
+# fi
 
 # install lunarvim
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+# bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 ./macos_settings.sh
 
 echo "Done!"
