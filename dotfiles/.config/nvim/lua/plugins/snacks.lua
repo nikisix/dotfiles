1a357bf80e38ae8059b535649ce41ea59c7653e2
Nick Tomasino <nick.w.tomasino@gmail.com>
2026-05-17 09:58:34 -0700

    unhiding files in explorer by default

---
+++ dotfiles/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+        hidden = true, -- affects most pickers
+    }
+    -- explorer = {
+    --   -- This shows hidden files (dotfiles) by default
+    --   hidden = true,
+    --   -- This also shows files ignored by git (optional)
+    --   ignored = true,
+    -- },
+  },
+}
