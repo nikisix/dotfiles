a35848fd2ba234e202c5c8a12e159212c77329e5
Nick Tomasino <nick.w.tomasino@gmail.com>
2026-05-23 01:32:00 -0700

    updating pyradio setup

--- apps/pyradio/setup.sh
+++ apps/pyradio/setup.sh
@@ -1,1 +1,5 @@
 mkdir -p ~/.pyradio && ln -s ./dotfiles/.config/pyradio/stations.csv ~/.pyradio/stations.csv
+mkdir ~/programs
+git clone git@github.com:coderholic/pyradio.git
+pipx install -e ~/programs/pyradio
+pipx inject -r ~/programs/pyradio/requirements_pipx.txt pyradio
