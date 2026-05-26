803372088747485e236f39fef8b6e4c0e9a1669f
Nick Tomasino <nick.w.tomasino@gmail.com>
2026-05-26 08:57:14 -0700

    cleaning up nvim plugins

--- dotfiles/.config/nvim/lua/plugins/markdown-preview.lua
+++ dotfiles/.config/nvim/lua/plugins/markdown-preview.lua
@@ -6,19 +6,22 @@ return {
     -- It supports live preview, custom CSS, and more.
     -- Note: This plugin requires Neovim 0.5 or higher.
     -- install without yarn or npm
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
-
-    -- install with yarn or npm
     -- "iamcco/markdown-preview.nvim",
     -- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    -- build = "cd app && yarn add",
-    -- init = function()
-    --     vim.g.mkdp_filetypes = { "markdown" }
-    -- end,
     -- ft = { "markdown" },
+    -- build = function() vim.fn["mkdp#util#install"]() end,
+
+-- install with yarn or npm
+-- if this doesn't work then manually run:
+    -- cd ~/.local/share/nvim/lazy/markdown-preview.nvim
+    -- npm install
+{
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+},
 }
--- dotfiles/.config/nvim/lua/plugins/snacks.lua
+++ dotfiles/.config/nvim/lua/plugins/snacks.lua
@@ -2,9 +2,10 @@ return {
   "folke/snacks.nvim",
   opts = {
     picker = {
-        hidden = true, -- affects most pickers
+        hidden = true,  -- affects most pickers
+        ignored = true, -- try moving these to files
     }
-    -- explorer = {
+    -- files = {
     --   -- This shows hidden files (dotfiles) by default
     --   hidden = true,
     --   -- This also shows files ignored by git (optional)
